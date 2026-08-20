040ef08ada26ddd2a4ecbce4eb8f7b6f01322301
Hezkiel09 <kielsiahaan09@gmail.com>
2026-08-20 22:11:33 +0700

    setup splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  
   runApp(const MyApp());
+
+  Future.delayed(const Duration(seconds: 2), (){
+    FlutterNativeSplash.remove();
+  });
 }
 
 class MyApp extends StatelessWidget {
